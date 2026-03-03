e8938252fc90357cdd7bb82cfb608de956ddd83e
ss1544-dotcom <ss1544@srmist.edu.in>
2026-03-03 11:29:38 +0530

    uploaded uc 1,2,3,4

---
+++ UC1.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * 
+ * This calss demonstrates A SIMPLE Java application that displays the
+ * OOPS acronym to the console.
+ * 
+ * @author Sameer 
+ * @version 1.0
+ */
+public class UC1 {
+
+    public static void main(String[] args){
+        System.out.println("OOPS");
+    }
+}
+// UC1 Implementation
---
+++ UC4.java
@@ -0,0 +1,58 @@
+
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        // Each banner line created using String.join()
+        String line1 = String.join("",
+                " *****    ",
+                " *****   ",
+                " ******   ",
+                " *****  ");
+
+        String line2 = String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     * ",
+                "*     * ");
+
+        String line3 = String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     * ",
+                "*       ");
+
+        String line4 = String.join("",
+                "*     *  ",
+                "*     *  ",
+                "******   ",
+                " *****  ");
+
+        String line5 = String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*        ",
+                "      * ");
+
+        String line6 = String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*        ",
+                "*     * ");
+
+        String line7 = String.join("",
+                " *****    ",
+                " *****   ",
+                "*        ",
+                " *****  ");
+
+        // Print banner
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
